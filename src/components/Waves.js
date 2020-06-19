a0b0e4527932b5ffb744e494d561298743c4e0d2
vedanshgarg <vedansh.garg.7@gmail.com>
2020-06-20 01:10:10 +0530

    re-added border

--- src/components/Waves.js
+++ src/components/Waves.js
@@ -5,6 +5,7 @@ import { rgba } from 'polished'
 import { useRaptoriTheme } from 'src/hooks'
 
 const Canvas = styled.canvas`
+	border-bottom: 5px solid var(--primary);
 	height: 100px;
 	vertical-align: bottom;
 	width: 100vw;
