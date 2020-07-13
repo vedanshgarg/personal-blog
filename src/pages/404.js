628537b15d67b20cb3e77f87e38bb256f368e0f3
vedanshgarg <vedansh.garg.7@gmail.com>
2020-07-14 02:13:18 +0530

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { Layout, SEO } from 'src/components'
+import { Layout, Link, SEO } from 'src/components'
 
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404: Not found" />
-		<h1>Not Found</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<h1>404 : Not Found</h1>
+		<p>Wanna learn about <Link to="https://httpstatuses.com/">HTTP Error Codes</Link> ?</p>
 	</Layout>
 )
 
