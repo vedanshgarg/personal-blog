48598db2ac7995558641992c79c195a9554cca53
vedanshgarg <vedansh.garg.7@gmail.com>
2020-07-14 02:22:49 +0530

    404 padding

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,11 @@ import { Layout, Link, SEO } from 'src/components'
 
 const NotFoundPage = () => (
 	<Layout>
-		<SEO title="404: Not found" />
-		<h1>404 : Not Found</h1>
-		<p>Wanna learn about <Link to="https://httpstatuses.com/">HTTP Error Codes</Link> ?</p>
+		<div style={{'padding': '40px 0px'}}>
+			<SEO title="404: Not found" />
+			<h1>404 : Not Found</h1>
+			<p>Wanna learn about <Link to="https://httpstatuses.com/">HTTP Error Codes</Link> ?</p>
+		</div>
 	</Layout>
 )
 
