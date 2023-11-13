90064b368ccca0ac516e717c122069f622290d2b
vedanshgarg <vedansh.garg.7@gmail.com>
2023-11-13 23:53:06 +0530

    add Zomato WorkExp

--- src/components/WorkExperience/workexp.js
+++ src/components/WorkExperience/workexp.js
@@ -1,5 +1,6 @@
-import CodingNinjas from "../../images/work-ex-icons/CodingNinjas.svg"
-import OyoRooms from "../../images/work-ex-icons/OyoRooms.svg";
+import CodingNinjas from '../../images/work-ex-icons/CodingNinjas.svg'
+import OyoRooms from '../../images/work-ex-icons/OyoRooms.svg'
+import Zomato from '../../images/work-ex-icons/Zomato.svg'
 
 const workexp = [
 	{
@@ -10,7 +11,8 @@ const workexp = [
 		date: 'May 2019 - July 2019',
 		title: 'Coding Ninjas',
 		subtitle: 'Software Development Intern',
-		description: 'Creative Direction, User Experience, Visual Design, Project Management, Team Leading'
+		description:
+			'Creative Direction, User Experience, Visual Design, Project Management, Team Leading',
 	},
 	{
 		id: 2,
@@ -20,18 +22,20 @@ const workexp = [
 		date: 'Jan 2020 - Jul 2021',
 		title: 'Oyo Rooms',
 		subtitle: 'Software Development Engineer',
-		description: 'Creative Direction, User Experience, Visual Design, Project Management, Team Leading'
+		description:
+			'Creative Direction, User Experience, Visual Design, Project Management, Team Leading',
 	},
 	{
 		id: 3,
-		iconSrc: OyoRooms,
-		background: '#ee2e24',
+		iconSrc: Zomato,
+		background: '#e23744',
 		iconPadding: '0px',
-		date: 'Aug 2021 - Present',
-		title: 'Oyo Rooms',
+		date: 'Nov 2021 - Present',
+		title: 'Zomato',
 		subtitle: 'Software Development Engineer 2',
-		description: 'Creative Direction, User Experience, Visual Design, Project Management, Team Leading'
+		description:
+			'Creative Direction, User Experience, Visual Design, Project Management, Team Leading',
 	},
-];
+]
 
-export default workexp;
+export default workexp
